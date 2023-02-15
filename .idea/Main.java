4e9cb34f69d13192b3f77d290a050989cc06c25d
Dávid <huszarikd@gmail.com>
2023-02-15 22:07:55 +0100

    a

--- .idea/Main.java
+++ .idea/Main.java
@@ -1,7 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(3);
-        System.out.println(358);
-        System.out.println(50000);
+        System.out.println(3+10);
     }
 }
